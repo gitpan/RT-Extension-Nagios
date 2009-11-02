d403564da27cea35cdbff414ec343ecb63193cc8
孙海军 <sunnavy@gmail.com>
2009-11-02 11:33:54 +0000

    Import of SUNNAVY/RT-Extension-Nagios-0.01 from CPAN.
    gitpan-cpan-distribution: RT-Extension-Nagios
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SUNNAVY/RT-Extension-Nagios-0.01.tar.gz
    gitpan-cpan-author:       SUNNAVY
    gitpan-cpan-maturity:     released

---
+++ lib/RT/Action/UpdateNagiosTickets.pm
@@ -0,0 +1,100 @@
+package RT::Action::UpdateNagiosTickets;
+
+use strict;
+use warnings;
+
+use base qw(RT::Action);
+
+sub Describe {
+    my $self = shift;
+    return ( ref $self );
+}
+
+sub Prepare {
+    return (1);
+}
+
+sub Commit {
+    my $self = shift;
+
+    my $attachment = $self->TransactionObj->Attachments->First;
+    return 1 unless $attachment;
+    my $new_ticket    = $self->TicketObj;
+    my $new_ticket_id = $new_ticket->id;
+
+    my $subject = $attachment->GetHeader('Subject');
+    return unless $subject;
+    if ( my ( $type, $category, $host, $problem_type, $problem_severity ) =
+        $subject =~
+        m{(PROBLEM|RECOVERY)\s+(Service|Host) Alert: ([^/]+)/(.*)\s+is\s+(\w+)}i
+      )
+    {
+        $RT::Logger->info(
+"Extracted type, category, host, problem_type and problem_severity from
+subject with values $type, $category, $host, $problem_type and $problem_severity"
+        );
+        my $tickets = RT::Tickets->new( $self->CurrentUser );
+        $tickets->LimitQueue( VALUE => $new_ticket->Queue )
+          unless RT->Config->Get('NagiosSearchAllQueues');
+        $tickets->LimitSubject(
+            VALUE    => "$category Alert: $host/$problem_type",
+            OPERATOR => 'LIKE',
+        );
+        $tickets->LimitStatus(
+            VALUE           => 'new',
+            OPERATOR        => '=',
+            ENTRYAGGREGATOR => 'or'
+        );
+        $tickets->LimitStatus(
+            VALUE           => 'open',
+            OPERATOR        => '=',
+            ENTRYAGGREGATOR => 'or'
+        );
+        $tickets->LimitStatus( VALUE => 'stalled', OPERATOR => '=' );
+        if ( RT->Config->Get('NagiosMergeTickets') ) {
+            while ( my $ticket = $tickets->Next ) {
+                next if $ticket->id == $new_ticket_id;
+                my ( $ret, $msg ) = $ticket->MergeInto($new_ticket_id);
+                if ( !$ret ) {
+                    $RT::Logger->error( 'failed to merge ticket '
+                          . $ticket->id
+                          . " into $new_ticket_id: $msg" );
+                }
+            }
+
+            if ( $type eq 'RECOVERY' ) {
+                my ( $ret, $msg ) = $new_ticket->Resolve();
+                if ( !$ret ) {
+                    $RT::Logger->error( 'failed to resolve ticket '
+                          . $new_ticket->id
+                          . ":$msg" );
+                }
+            }
+        }
+        elsif ( $type eq 'RECOVERY' ) {
+            while ( my $ticket = $tickets->Next ) {
+                my ( $ret, $msg ) = $ticket->Comment(
+                    Content => 'going to be resolved by ' . $new_ticket_id,
+                    Status => 'resolved',
+                    );
+                if ( !$ret ) {
+                    $RT::Logger->error(
+                        'failed to comment ticket ' . $ticket->id . ": $msg" );
+                }
+
+                my ( $ret, $msg ) = $ticket->Resolve();
+                if ( !$ret ) {
+                    $RT::Logger->error(
+                        'failed to resolve ticket ' . $ticket->id . ": $msg" );
+                }
+            }
+            my ( $ret, $msg ) = $new_ticket->Resolve();
+            if ( !$ret ) {
+                $RT::Logger->error(
+                    'failed to resolve ticket ' . $new_ticket->id . ":$msg" );
+            }
+        }
+    }
+}
+
+1;
---
+++ lib/RT/Extension/Nagios.pm
@@ -0,0 +1,67 @@
+use warnings;
+use strict;
+
+package RT::Extension::Nagios;
+
+=head1 NAME
+
+RT::Extension::Nagios - Merge and resolve Nagios tickets
+
+=cut
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 DESCRIPTION
+
+Based on http://wiki.bestpractical.com/view/AutoCloseOnNagiosRecoveryMessages,
+thanks, Todd Chapman!
+
+Nagios( L<http://www.nagios.org> ) is a powerful monitoring system that enables
+organizations to identify and resolve IT infrastructure problems before they
+affect critical business processes.
+
+Once you create Nagios tickets by piping Nagio's email notifications, this
+extension helps you merge and resolve them.
+
+We identify email by its subject, so please keep it as the
+default one or alike, i.e. subject should pass the regex:
+
+C<<< qr{(PROBLEM|RECOVERY)\s+(Service|Host) Alert: ([^/]+)/(.+)\s+is\s+(\w+)}i >>>
+
+e.g.  "PROBLEM Service Alert: localhost/Root Partition is WARNING":
+
+There are 5 useful parts in subject( we call them type, category, host,
+problem_type and problem_severity ):
+
+PROBLEM, Service, localhost, Root Partition and WARNING
+
+( Currently, we don't make use of problem_severity actually )
+
+After the new ticket is created, the following is done:
+
+1. find all the other active tickets in the same queue( unless
+C<<< RT->Config->Get('NagiosSearchAllQueues') >>> is true, which will cause
+to search all the queues ) with the same values of $category, $host and
+$problem_type.
+
+2. if C<< RT->Config->Get('NagiosMergeTickets') >>> is true, merge all of them
+into the new ticket, if $type is 'RECOVERY', resolve the new created ticket.
+
+if C<< RT->Config->Get('NagiosMergeTickets') >>> is false and $type is
+'RECOVERY', resolve all the found tickets and the new created ticket.
+
+=head1 AUTHOR
+
+sunnavy  C<< <sunnavy@bestpractical.com> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+RT-Extension-Nagios is Copyright 2009 Best Practical Solutions, LLC.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
